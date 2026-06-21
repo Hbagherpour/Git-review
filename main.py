1b16c7459695bb02025b9c617ffa3e5d739c9113
Hossein Bagherpour <ieh.bagherpour@gmail.com>
2026-06-21 18:08:43 +0300

    first commit - adding the main python file to the repo

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+This is file is describing the song Ring my bells by Enrique.
+"""
+Name = "Ring my bells"
+Artist = "Enrique lglesias"
+Genre = "Disco"
+Year_released = 1979
+Length = 4.28
+# the duration has been written in minutes.
+
+print(Name)
+print(Artist)
+print(Genre)
+print(Year_released)
+print(Length)
