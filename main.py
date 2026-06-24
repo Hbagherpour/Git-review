5179fe0e8229e674a3b57547826e5d414955c021
Hossein Bagherpour <ieh.bagherpour@gmail.com>
2026-06-24 21:48:47 +0300

    updated the year releas of album

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ This is file is describing the song Ring my bells by Enrique.
 Name = "Ring my bells"
 Artist = "Enrique lglesias"
 Genre = "Disco"
-Year_released = 2000
+Year_released = 1979
 Length = 4.28
 # the duration has been written in minutes.
 
